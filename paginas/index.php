5022d46bdd5a81b1d19334eec0c46ed07ac6bdb1
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2023-10-19 00:04:49 -0300

    cambios nav-bar

--- paginas/index.php
+++ paginas/index.php
@@ -9,42 +9,50 @@
     <link rel="stylesheet" href="style-perfil.css">
   </head>
   <body>
-  <div class="row bg-light">
-    <div class="col-10 offset-1">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <img src="main-logo.svg" class="img-fluid logo" alt="logo">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item ">
-                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Mis vehiculos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Perfil</a>
-              </li>
-              <li class="nav-item ml">
-                <a class="nav-link active" aria-current="page" href="#">Cerrar sesión</a>
-              </li>
-            </ul>
-
-          </div>
-        </div>
-      </nav>
-    </div>
-
-    <div class="container-fluid fondo-perfil">
-        <div class = "row">
-            <div class = "col-4">
-              
+    <div class="container-fluid">
+      <div class="row bg-light">
+      <div class="col-10 offset-1">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <img src="main-logo.svg" class="img-fluid logo" alt="logo">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+              aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item ">
+                  <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Mis vehiculos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Perfil</a>
+                </li>
+                <li class="nav-item ml">
+                  <a class="nav-link active" aria-current="page" href="#">Cerrar sesión</a>
+                </li>
+              </ul>
 
             </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container text-center mt-5">
+        <div class = "row " >
+            <div class="col-4 mt-8">
+              <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+              
+            </div>
+            <div class="col">
+              <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+              <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+            </div>
+            <div class="col"><div class="p-3 mb-2 bg-primary text-white">.bg-primary</div></div>
+        
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
