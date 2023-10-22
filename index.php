765def19a41d03f93c2f670bf2854ca9fcaa733f
TomasFellTapia <tfell@ing.ucsc.cl>
2023-10-22 14:19:53 -0300

    dsdf
    dada

--- index.php
+++ index.php
@@ -8,33 +8,33 @@
     <link rel="stylesheet" href="disnio.css">
   </head>
   <body>
-    <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="https://ucsc.cl/"><img src="logo.png" alt=""></a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          >
-          <a class="nav-link" href="#">Link</a>
-          
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <div class="vinculos">
-      <a href="#" >Inicio</a>
-      </div>
+  <div class="row bg-light"> <!-- nav bar -->
+    <div class="col-10 offset-1">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <img src="img/logo.png" class="img-fluid logo" alt="logo">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#">Admin1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Mis Cerrar Secion</a>
+              </li>
+              
+              
+            </ul>
+
+          </div>
+        </div>
+      </nav>
     </div>
   </div>
-</nav>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  </div>
   </body>
