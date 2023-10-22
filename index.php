8d8210f18e300e5ac7a5f36dd807a3de32a385c3
TomasFellTapia <tfell@ing.ucsc.cl>
2023-10-22 09:37:58 -0300

    v
    vv

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="disnio.css">
   </head>
   <body>
     <div class="container-fluid">
@@ -20,13 +21,17 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
+          >
           <a class="nav-link" href="#">Link</a>
+          
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
+      <div class="vinculos">
       <a href="#" >Inicio</a>
+      </div>
     </div>
   </div>
 </nav>
