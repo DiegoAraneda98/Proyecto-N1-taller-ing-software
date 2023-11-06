5e9bfc9b6100badafe5c8f78fb0a3601a00fe411
TomasFellTapia <tfell@ing.ucsc.cl>
2023-11-06 08:06:41 -0300

    b
    fsf

--- index.php
+++ index.php
@@ -3,38 +3,50 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Encuesta</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="disnio.css">
   </head>
   <body>
-  <div class="container-fluid">
-  <div class="row bg-light"> <!-- nav bar -->
-    <div class="col-10 offset-1">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <img src="img/logo.png" class="img-fluid logo" alt="logo">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item ">
-                <a class="nav-link active" aria-current="page" href="#">Admin1</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Mis Cerrar Secion</a>
-              </li>
-              
-              
-            </ul>
-
-          </div>
+    <div class="container-fluid">
+      <div class="row">
+       <form action="añadir_contacto.php" method="post">
+        <div class="iz">
+          <h1>Encuesta:</h1>
+            Nombre:
+            <input type="text" name="Nombre" id="">
+            RUT:
+            <input type="text" name="Rut" id="">
+            correo:
+            <input type="email" name="correo" id="">
+            Telefono:
+            <input type="text" name="Telefono" id="">
+            Sujerencia:
+            <input type="text" name="sujerencia" id="">
         </div>
-      </nav>
-    </div>
-  </div>
+        <div class="de">
+          Interfaz:
+          <select  id="inter" class="form-control">
+            <option value="5">Muy Buena</option>
+            <option value="4">Buena</option>
+            <option value="3">Regular</option>
+            <option value="2">Mala</option>
+            <option value="1">Muy Mala Mala</option>
 
-  </div>
+          </select>
+          Velocidad de respuesta:
+          <select  id="vel" class="form-control">
+            <option value="5">Muy Rapida</option>
+            <option value="4">Rapida</option>
+            <option value="3">Regular</option>
+            <option value="2">Lenta</option>
+            <option value="1">Lenta</option>
+
+          </select>
+        </div>
+       </form>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
+</html>
