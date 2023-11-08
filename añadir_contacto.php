8acf0821c738db38cbe8e88a49f1f5caad14e627
TomasFellTapia <tfell@ing.ucsc.cl>
2023-11-08 13:11:02 -0300

    dgd
    dfgdda

--- añadir_contacto.php
+++ añadir_contacto.php
@@ -1,3 +1,8 @@
-<?php 
+<?php include("..//BaseDatos/safeconectar.php");
+$einter = $POST_["interfaz"];
+$erapidez = $POST_["rapidez"];
+$rut = $POST_["inputrut"];
+$query = "INSERT INTO encuestas (rapidez, interfaz, id_usuario) VALUES ('$erapidez', '$einter', '$rut');";
 
+$result =  mysqli_query($connection, $query);
 ?>
--- index.php
+++ index.php
@@ -9,57 +9,62 @@
   </head>
   <body>
     <div class="container-fluid fondo">
-    <form action="añadir_contacto.php" method="post">  
-    <div class="row">
-       
-        <div class="col-md-4">
-          <h1>Encuesta:</h1>
-            Nombre:
-            <input type="text" name="Nombre" id="">
-            <br>
-            RUT:
-            <input type="text" name="Rut" id="">
-            <br>
-            correo:
-            <input type="email" name="correo" id="">
-            <br>
-            Telefono:
-            <input type="text" name="Telefono" id="">
-            <br>
-            <br>
-            Sujerencia:
-            <br>
-            <textarea name="sug" id="suge" cols="55" rows="4"></textarea>
-        </div>
-          <div class="row">
-          <div class="col-md-4">
-          Interfaz:
-          <select  id="inter" class="form-control">
-            <option value="5">Muy Buena</option>
-            <option value="4">Buena</option>
-            <option value="3">Regular</option>
-            <option value="2">Mala</option>
-            <option value="1">Muy Mala Mala</option>
-
-          </select>
-          Velocidad de respuesta:
-          <select  id="vel" class="form-control">
-            <option value="5">Muy Rapida</option>
-            <option value="4">Rapida</option>
-            <option value="3">Regular</option>
-            <option value="2">Lenta</option>
-            <option value="1">Lenta</option>
-
-          </select>
-          <div class="col-md-12">
-          <button type="button" class="btn btn-info" id="saveForm" onclick="saveForm">Guardar Encuesta</button>
-        <button type="button" class="btn btn-danger" id="clearForm">Limpiar formulario</button>
-          </div>
-          </div>
-        </div>
-       </form>
+    <form>
+  <div class="row mb-3">
+    <label for="inputnombre" class="col-sm-2 col-form-label">Nombre: </label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputnombre">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputRut" class="col-sm-2 col-form-label">RUT:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputrut">
+    </div>
+    <div class="row mb-3">
+    <label for="inputCorreo" class="col-sm-2 col-form-label">E-Mail:</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputema">
+    </div>
+     <div class="row mb-3">
+    <label for="inputTele" class="col-sm-2 col-form-label">Telefono:</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="inputtel">
+    </div>
+     <div class="row mb-3">
+    <label for="inputSug" class="col-sm-2 col-form-label">Sugerencias:</label>
+    <div class="col-sm-10">
+      <textarea name="tasug" id="inputsuge" cols="50" rows="10" maxlength="500"></textarea>
+    </div>
+  </div>
+  
+  <div class="row mb-3">
+    <div class="col-sm-10 offset-sm-2">
+      <div class="form-check">
+        Interfaz:
+        <Select id="interfaz">
+        <option value="Muy Buena">Muy buena</option>
+        <option value="Buena">Buena</option>
+         <option value="Neutral">Neutral</option>
+        <option value="Mala">Mal</option>
+        <option value="Muy Mala">Muy Mala</option>
+        </Select>
+         Velocidad Reaccion:
+         <Select id="rapidez">
+        <option value="Muy Rapida">Muy Rapida</option>
+        <option value="Rapida">Rapida</option>
+         <option value="Neutral">Neutral</option>
+        <option value="Lenta">Lenta</option>
+        <option value="Muy Lenta">Muy Lenta</option>
+        </Select>
+        </label>
       </div>
     </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+  <button disabled="disabled">Borrar</button>
+  
+</form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
