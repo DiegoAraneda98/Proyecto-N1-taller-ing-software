bfde839ffe84546c1824d3f3ab8d2b5ba2f6bfb9
nparrac1 <npcruz630@gmail.com>
2023-11-11 19:16:23 -0300

    update_delete

--- verificaciones.js
+++
@@ -1,6 +0,0 @@
-
-    function filtro() {
-        var tecla = event.key;
-        if (!['0', '1', '2','3', '4', '5', '6', '7', '8', '9', 'Backspace'].includes(tecla))
-            event.preventDefault()
-    }
