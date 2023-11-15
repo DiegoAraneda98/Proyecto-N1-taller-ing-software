0e6f8da561f51e74ee917c6de1ac14573d015786
TomasFellTapia <tfell@ing.ucsc.cl>
2023-11-14 22:09:53 -0300

    sfsdfsd
    fsdfsfs

--- BaseDatos/conectar.php
+++ BaseDatos/conectar.php
@@ -1,3 +1,11 @@
 <?php 
-    $connection = mysqli_connect("localhost","root","","repuestos");
+
+
+$conexion = mysqli_connect("localhost","root","","safedrive");
+
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
 ?>
--- añadir_contacto.php
+++ añadir_contacto.php
@@ -5,7 +5,7 @@ $rut = $POST_["inputrut"];
 $nombre_re = $POST_["inombre"];
 $correo_re = $POST_["icorreo"];
 $tele_re = $POST_["itelefono"];
-$sug = = $POST_["iSugerencia"]
-$query = "INSERT INTO encuestas (rapidez, interfaz, id_usuario, nombre, correo, telefono, sugerencia) VALUES ('$erapidez', '$einter', '$rut', '$nombre_re','$correo_re','$tele_re','$sug');";
+$sug =  $POST_["iSugerencia"]
+$sql = "INSERT INTO encuestas (rapidez, interfaz, id_usuario) VALUES ('$erapidez', '$einter', '$rut');";
 $result =  mysqli_query($connection, $query);
 ?>
