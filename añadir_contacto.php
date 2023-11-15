86dd1eea5f12de11c3fe8689792aac6d1e531877
TomasFellTapia <tfell@ing.ucsc.cl>
2023-11-14 21:47:58 -0300

    version 6.5
    version 6.5

--- añadir_contacto.php
+++ añadir_contacto.php
@@ -2,7 +2,10 @@
 $einter = $POST_["interfaz"];
 $erapidez = $POST_["rapidez"];
 $rut = $POST_["inputrut"];
-$query = "INSERT INTO encuestas (rapidez, interfaz, id_usuario) VALUES ('$erapidez', '$einter', '$rut');";
-
+$nombre_re = $POST_["inombre"];
+$correo_re = $POST_["icorreo"];
+$tele_re = $POST_["itelefono"];
+$sug = = $POST_["iSugerencia"]
+$query = "INSERT INTO encuestas (rapidez, interfaz, id_usuario, nombre, correo, telefono, sugerencia) VALUES ('$erapidez', '$einter', '$rut', '$nombre_re','$correo_re','$tele_re','$sug');";
 $result =  mysqli_query($connection, $query);
 ?>
