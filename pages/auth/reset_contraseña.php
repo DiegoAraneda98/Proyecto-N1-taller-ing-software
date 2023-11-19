9d356ba231158b7aae203c605955bddd628c0b1a
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2023-11-18 21:33:57 -0300

    actualizacion
    cambios

--- pages/auth/reset_contraseña.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <form action="index.php?p=auth/actions/restablecer_contraseña" method="post">
-        <input type="email" name="correo">
-        <button type="submit">Mandar</button>
-    </form>
-</div>
