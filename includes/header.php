5ec30904113f9d4d324009dbaa2869b0afbc148e
nparrac1 <npcruz630@gmail.com>
2023-11-21 20:27:56 -0300

    update guardia
    se incluyo guardia actualizado con camara siempre prendida, falta hora salida .

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,8 @@
 <!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
