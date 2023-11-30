82bd1ab83f0bdf6f1b8fb4b650640cc6caf8b202
nparrac1 <npcruz630@gmail.com>
2023-11-30 14:38:32 -0300

    se actualizo guardia para que se ingrese en usuarios

--- js/admin_editar_guardia.js
+++ js/admin_editar_guardia.js
@@ -3,11 +3,14 @@ $(document).ready(function () {
     $(document).on('click', 'a[data-role=update_tipo]', function () {
         var id = $(this).data('id_tipo');
         var nombre = $('#' + id).children('td[data-target=nombre]').text();
+        var contraseña = $('#' + id).children('td[data-target=contraseña]').text();
         var id_caseta = $('#' + id).children('td[data-target=id_caseta]').text();
-        console.log("ID:", id);
+        /* console.log("ID:", id);
         console.log("Nombre:", nombre);
         console.log("Caseta:", id_caseta);
+        console.log("Caseta:", contraseña); */
         $('#nombre').val(nombre);
+        $('#contraseña').val(contraseña);
         $('#id_caseta').val(id_caseta);
         $('#id_tipo').val(id);
         $('#editarmodalGuardia').modal('toggle');
@@ -18,19 +21,21 @@ $(document).ready(function () {
 $('#save_tipo').click(function () {
     var id = $('#id_tipo').val();
     var nombre = $('#nombre').val();
+    var contraseña = $('#contraseña').val();
     var id_caseta = $('#id_caseta').val();
-    console.log("ID:", id);
-    console.log("Nombre:", nombre);
+   /*  console.log("ID:", id);
+    console.log("Nombre:", nombre); */
     $.ajax({
         url: 'index.php?p=admin/actions/actions_guardia/admin_updateGuardia',
         method: 'post',
-        data: { nombre: nombre, id: id , id_caseta:id_caseta},
+        data: { nombre: nombre, contraseña: contraseña , id: id , id_caseta:id_caseta},
         success: function (response) {
             if (response.startsWith("Error")) {
                 console.log("Respuesta del servidor:", response);
             } else {
                 
                 $('#' + id).children('td[data-target=nombre]').text(nombre);
+                $('#' + id).children('td[data-target=contraseña]').text(contraseña);
                 $('#' + id).children('td[data-target=id_caseta]').text(id_caseta);
                 $('#editarmodalGuardia').modal('toggle');
             }
